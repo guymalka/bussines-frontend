8e57feac48989f3fb10d8b0fcda2e7616f8cbba3
guymalka <guy.malka@gmail.com>
2019-12-29 17:53:27 +0200

    design

--- src/components/RetailerCoupons.js
+++ src/components/RetailerCoupons.js
@@ -5,6 +5,10 @@ import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
 import { CustomObject } from '../components/common.js'
 
+const CheckBoxLiItem = ({ name, value, selectItem }) => {
+    return <li key={value}  ><input onChange={ selectItem } value={value} type="checkbox" /><span> { name } </span></li>
+}
+
 
 const txtFieldState = {
     value: "",
@@ -32,30 +36,115 @@ const Field = ({ valid, type, fieldId, fieldName, typeMismatch, formatErrorTxt, 
     
 }
 
-
+/**
+ * main class, holding components
+ */
 class RetailerCoupons extends React.Component  {
-    
+    constructor(props){
+        super(props);
+        this.openloyaltiesForm = this.openloyaltiesForm.bind(this);
+        this.saveCouponLoyaltyRelations = this.saveCouponLoyaltyRelations.bind(this);
+        this.getSelectedCoupons = this.getSelectedCoupons.bind(this);
+    }
     state = {
         displayForm: false,                
+        displayLoyaltiesForm: false,
+        selectedCoupon: null,
+        selectedCoupons: []
     }
     openForm(){
         this.setState({displayForm: true});
     }
+    getSelectedCoupons(coupons){
+        console.log(coupons);
+        this.setState({displayLoyaltiesForm: false,
+            selectedCoupons: coupons
+        })
+    }
+    openloyaltiesForm(coupon){
+        console.log(coupon)
+        this.setState({displayLoyaltiesForm: true,
+            selectedCoupon: coupon
+        });
+    }
+    saveCouponLoyaltyRelations(selectedLoyalties, assign){        
+        let data = {};
+        data.selectedLoyalties = selectedLoyalties;
+        data.SelectedCoupons = this.state.selectedCoupons;
+        data.assign = assign;
+        fetch(`${process.env.REACT_APP_NOT_SECRET_CODE}/api/coupons/manageloyaltyrelations`, {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify(data)
+        })
+
+    }
     render(){
+        let { displayForm, displayLoyaltiesForm, selectedCoupon } =  this.state;
         return <div>
-                 <div className={ this.state.displayForm ? " split right" : "" }> 
+                 <div className={ (displayForm || displayLoyaltiesForm ) ? " split right" : "" }> 
                  <h2 className="header" >קופונים</h2>
                 <input  className="btn btn-success"
                     onClick={ () =>  this.openForm()} value="פתח טופס" type="button" />
-                    <CouponsTable />
+                   <div >  <CouponsTable setSelectedCoupons={ this.getSelectedCoupons }  manageLoyalties={this.openloyaltiesForm} /></div>
                 </div>                           
-                { this.state.displayForm ? 
-                    <CouponsForm closeForm={ () => { this.setState({displayForm : false }) } } /> : 
-                    <div></div>
-                }
+                    <div className={ (displayForm || displayLoyaltiesForm ) ?  " split2 left" : "" } >
+                        <div className="centered" >
+                            { displayForm ? <CouponsForm closeForm={ () => { this.setState({displayForm : false }) } } /> 
+                            : displayLoyaltiesForm ?  <LoyaltiesForm sendRelationChanges={this.saveCouponLoyaltyRelations} coupon={selectedCoupon} /> : <div></div> }
+                        </div>
+                    </div>                
                 </div>
     }
 
+}
+
+class LoyaltiesForm extends React.Component{
+    constructor(props){
+        super(props);
+        this.onCheckboxChange = this.onCheckboxChange.bind(this);
+    }
+
+    state = {
+        loyalties: [],
+        selectedLoyalties: []
+    }
+    onCheckboxChange(item){
+        if (item.target.checked)
+            this.setState( { selectedLoyalties:  this.state.selectedLoyalties.concat(item.target.value) })
+        else
+            this.setState({ selectedLoyalties: this.state.selectedLoyalties.filter( m => m != item.target.value  )})    
+    }
+    componentDidMount(){
+        //todo: change to dynamic
+
+        fetch(`${process.env.REACT_APP_NOT_SECRET_CODE}/api/retailerloyalty/3`)
+            .then(res => res.json())
+            .then(res => {                                                               
+                let loyaltiesList = res.map(m => <CheckBoxLiItem selectItem={this.onCheckboxChange} name={m.name}  value={m.loyalty_id}  /> )
+                this.setState({ loyalties: loyaltiesList });
+            });
+    }
+    render(){
+        
+        
+        return  <form style={{ paddingTop: '200' }} >
+            <ul>
+                {this.state.loyalties}
+            </ul>
+            <div style={{ display: 'flex', flexDirection:"row" }} >
+                <div>
+                    <input type="button" value="שייך למועדונים מסומנים" onClick={ () => { this.props.sendRelationChanges(this.state.selectedLoyalties, true)  } } className="btn btn-info" />
+                </div>
+                <div>
+                    <input type="button" value="בטל שיוך למועדונים" onClick={ () => { this.props.sendRelationChanges(this.state.selectedLoyalties, false)  } } className="btn btn-info" />
+                </div>
+            </div>
+            </form>        
+    }
 }
 
 class CouponsForm extends React.Component  {
@@ -67,6 +156,7 @@ class CouponsForm extends React.Component  {
         item_id:    { ...txtFieldState, fieldName: "מספר מוצר", required: true, requiredTxt: "Last Name is required", type: "number" },
         restrictions: { ...txtFieldState, fieldName: "תנאי הקופון", required: true, requiredTxt: "תיאור הוא שדה חובה", type: "text" },
         duration:    { ...txtFieldState, fieldName: "מספר ימים", required: false, type: "number" },
+        file:    { ...txtFieldState, fieldName: "מספר ימים", required: false, type: "number" },
         allFieldsValid: false
     }
 
@@ -167,7 +257,7 @@ class CouponsForm extends React.Component  {
         const inputFormDisplay = !allFieldsValid ? "block" : "none";
         const fields = this.mapFieldInputs();
         const renderFields = fields.map(x => <Field {...x} />);
-
+        console.log('rendrt coupon form');    
         return <div className="left" >
                 <form
                         className="form-inside-input"
@@ -190,11 +280,7 @@ class CouponsTable extends React.Component  {
         this.getCouponsByLoyalty = this.getCouponsByLoyalty.bind(this);
         this.getCouponsByRetailer = this.getCouponsByRetailer.bind(this);
         this.selectQuery = this.selectQuery.bind(this);
-        this.removeCoupon = this.removeCoupon.bind(this);
-        this.assignCoupon = this.assignCoupon.bind(this);
-        //this.checkbox = this.checkbox.bind(this);
-
-        //this.changeCheckbox = this.changeCheckbox.bind(this);
+        this.manageCoupons = this.manageCoupons.bind(this);
     }
 
     state = {
@@ -206,7 +292,7 @@ class CouponsTable extends React.Component  {
                         <input
                             type="checkbox"
                             className="checkbox"
-                            checked={this.state.selected[original.coupon_id] === true}
+                            checked={  this.state.selected[original.coupon_id] && this.state.selected[original.coupon_id].value == true }
                             onChange={() => this.toggleRow(original.coupon_id)}
                         />
                     );
@@ -255,7 +341,7 @@ class CouponsTable extends React.Component  {
             }
 
         ],
-        selected: new CustomObject(), 
+        selected: [] , // new CustomObject(), 
         selectAll: 0,
         queryTypes : [{"value":1,"label":"כל הקופונים"},{"value":2,"label":"לפי מועדון"}],
         selectedQuery:{"value":1,"label":"כל הקופונים"}
@@ -264,11 +350,19 @@ class CouponsTable extends React.Component  {
     componentDidMount(){
         this.getCouponsByRetailer();
     }
-    toggleRow(coupon_id) {
-		const newSelected = Object.assign({}, this.state.selected);
-		newSelected[coupon_id] = !this.state.selected[coupon_id];
+    toggleRow(coupon_id) {        
+        let newSelected = [...this.state.selected];  //Object.assign({}, this.state.selected);
+        if (!this.state.selected[coupon_id])
+            newSelected.push({ coupon: coupon_id, value: true}); // = !this.state.selected[coupon_id];
+        else
+            newSelected = newSelected.filter((val) => {
+                return val != coupon_id;
+            });
+        
+
+        this.props.setSelectedCoupons(newSelected);
 		this.setState({
-			selected: newSelected,
+			selected: [...newSelected],
 			selectAll: 2
 		});
 	}
@@ -299,7 +393,9 @@ class CouponsTable extends React.Component  {
         var items = Object.keys(value);
         return items;
     }
-    removeCoupon(){
+    manageCoupons(){
+        this.props.manageLoyalties(this.state.selected);
+        /*
         let items =this.getCouponId(this.state.selected)
         console.dir ( items  );
         fetch(`${process.env.REACT_APP_NOT_SECRET_CODE}/api/coupons/remove`, {
@@ -310,7 +406,9 @@ class CouponsTable extends React.Component  {
             method: "DELETE",
             body: JSON.stringify(items)
         })
+        */
     }
+    /*
     assignCoupon(){
         fetch(`${process.env.REACT_APP_NOT_SECRET_CODE}/api/coupons/assign`, {
             headers: {
@@ -321,6 +419,7 @@ class CouponsTable extends React.Component  {
             body: JSON.stringify(this.state.selected)
         })
     }
+    */
     getCouponsByLoyalty(){
         fetch(`${process.env.REACT_APP_NOT_SECRET_CODE}/api/coupons/retailer-loyalty-coupons`)
             .then(res => res.json())
@@ -328,10 +427,9 @@ class CouponsTable extends React.Component  {
                 this.setState({ data: res });
             });
     }
-    render(){
-        console.dir(this.state.selected)
+    render(){        
         const { data, columns, queryTypes, selected } = this.state;
-        
+        console.log(this.state.selected);
         return <div>
                 <div className="form-row"  > 
                     <div className="form-group col-md-2" >
@@ -339,12 +437,9 @@ class CouponsTable extends React.Component  {
                     </div>
                     <div className="form-group col-md-2" >
                         <input disabled={ selected == {} } className="btn btn-success"
-                        onClick={ () =>  this.removeCoupon()} value=" הסר ממועדון" type="button" />
+                        onClick={ () =>  this.manageCoupons()} value=" שיוך קופונים למועדון" type="button" />
                         </div>
-                    <div className="form-group col-md-2" >
-                        <input disabled={ selected == {} }  className="btn btn-success"
-                        onClick={ () =>  this.assignCoupon()} value="הקצה למועדון" type="button" />
-                        </div>
+                    <div className="form-group col-md-2" ></div>
                     <div className="form-group col-md-6" ></div>
                  </div>                 
                  <div className="form-row" >
